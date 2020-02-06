5e5a222b5f9f99e87a0ac275b9cbeb05f072a9b7
Digital Villa <digitalvilla01@gmail.com>
2020-02-05 22:13:21 -0700

    Auth created

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { navigate } from "gatsby";
+import IdentityModal from "react-netlify-identity-widget";
 import { Router } from "@reach/router";
 import Layout from "../components/Layout.jsx";
 import Profile from '../components/Profile.jsx'
@@ -7,7 +8,10 @@ import Base from "../routes/Base.jsx";
 import Secret from "../routes/Secret.jsx";
 import Login from "../routes/Login.jsx";
 
+import 'react-netlify-identity-widget/styles.css';
+
 const Dashboard = ({ location }) => {
+	const [isVisible, setVisibility] = useState(false);
 
 	useEffect(() => {
 		if (location.pathname.match(/^\/dashboard\/?$/)) {
@@ -15,14 +19,21 @@ const Dashboard = ({ location }) => {
 		}
 	}, []);
 
+	// Helper functions
+	const showModal = () => setVisibility(!isVisible);
+
 	return (
 		<Layout>
 			<Profile />
 			<Router>
 				<Base path='/dashboard/base' />
 				<Secret path='/dashboard/secret' />
-				<Login path='/dashboard/login' />
+				<Login path='/dashboard/login' showModal={showModal} />
 			</Router>
+			<IdentityModal
+				showDialog={isVisible}
+				onCloseDialog={showModal}
+			/>
 
 			<p>TODO: Create Dashboard</p>
 		</Layout>
--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { useIdentityContext } from "react-netlify-identity";
+import { navigate } from "gatsby";
 
+const Login = ({ showModal }) => {
+	const identity = useIdentityContext();
 
-const Login = () => {
+	if ( identity && identity.isLoggedIn ) {
+		navigate('/dashboard/secret', { replace: true });
+	}
+
 	return (
 		<>
 			<h1>Log In or Sign Up</h1>
-			<button>Log In</button>
+			<button onClick={showModal}>Log In</button>
 		</>
 	)
 }
